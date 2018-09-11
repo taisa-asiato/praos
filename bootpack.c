22fcf3cba20fc920903dc9a8a04717902a5679d5
taisa-asiato <taisa.aikou@gmail.com>
2018-09-12 00:37:10 +0900

    [MOD] day24 21d

--- bootpack.c
+++ bootpack.c
@@ -287,6 +287,16 @@ void HariMain(void)
 											mmx = mx;
 											mmy = my;
 										}
+										if ( sht->bxsize - 21 <= x && x < sht->bxsize - 5 && 5 <= y && y < 19 ) {
+											/* [x]ボタンクリック */
+											if ( sht->task != 0 ) {	/* アプリが作ったウィンドウか */
+												cons = ( struct CONSOLE * ) *( ( int * ) 0x0fec );
+												cons_putstr0( cons, "\nBreak(mouse) :\n" );
+												io_cli();
+												task_cons->tss.eax = ( int ) & ( task_cons->tss.esp0 );
+												task_cons->tss.eip = ( int ) & asm_end_app;
+											}
+										}
 										break;
 									}
 								}
