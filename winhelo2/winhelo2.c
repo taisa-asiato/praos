2639ff4c420773e421ea998f072e6c4a18784c8b
taisa-asiato <taisa.aikou@gmail.com>
2018-12-24 18:07:15 +0900

    [mod] day28 25a

--- winhelo2/winhelo2.c
+++ winhelo2/winhelo2.c
@@ -5,13 +5,18 @@ void api_putstrwin(int win, int x, int y, int col, int len, char *str);
 void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col);
 void api_end(void);
 */
-char buf[150 * 50];
 
 void HariMain(void)
 {
 	int win;
+	char buf[150 * 50];
 	win = api_openwin(buf, 150, 50, -1, "hello");
 	api_boxfilwin(win,  8, 36, 141, 43, 3 /* â© */);
 	api_putstrwin(win, 28, 28, 0 /* çï */, 12, "hello, world");
+	for ( ; ; ) {
+		if ( api_getkey( 1 ) == 0x0a ) {
+			break;
+		}
+	}
 	api_end();
 }
